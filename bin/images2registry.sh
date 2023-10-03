477d073a9bbf88e94ff2197c6a9131f53216c6b3
Oleg Sadov <oleg.sadov@gmail.com>
2023-10-03 11:21:30 +0300

    Fixed docker image convertor script
    Signed-off-by: Oleg Sadov <oleg.sadov@gmail.com>

--- bin/images2registry.sh
+++ bin/images2registry.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+#TAG='2023.1-ubuntu-jammy'
+TAG='2023.1-rocky-9'
+#TAG='master-rocky-9'
+
 docker images | grep '^kolla\/' | awk '{print $1,$2}' | while read -r image tag
 do
     new_image_name=${image#"kolla/"}
-    echo docker tag ${image}:${tag} 'quay.io/openstack.kolla/'${new_image_name}:2023.1-rocky-9 && \
-    docker tag ${image}:${tag} 'quay.io/openstack.kolla/'${new_image_name}:2023.1-rocky-9 # && \
-    #echo docker push quay.io/openstack.kolla/${new_image_name}:2023.1-ubuntu-jammy && \
-    #docker push quay.io/openstack.kolla/${new_image_name}:2023.1-ubuntu-jammy
+    echo docker tag ${image}:${tag} 'quay.io/openstack.kolla/'${new_image_name}:${TAG} && \
+    docker tag ${image}:${tag} 'quay.io/openstack.kolla/'${new_image_name}:${TAG}
 done
